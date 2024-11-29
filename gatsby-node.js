390415d743e3476ab3492a2a24d66f3413c23ced
José Antonio Cruz Soto <antonio.cruz@vantis.mx>
2024-11-29 16:20:22 -0600

    [feat] project of congratulations

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,10 +9,13 @@
  */
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
+
+  // page.matchPath is a special key that's used for matching pages
+  // only on the client.
+  if (page.path.match(/^\/app/)) {
+    page.matchPath = "/app/*"
+
+    // Update the page.
+    createPage(page)
+  }
 }
